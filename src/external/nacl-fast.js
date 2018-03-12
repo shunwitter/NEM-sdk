9d9ae98e2180190c34fd73129af36dae6804dc57
shunwitter <polytechnics.shun@gmail.com>
2018-03-12 09:26:05 +0900

    Detect react-native

--- src/external/nacl-fast.js
+++ src/external/nacl-fast.js
@@ -727,7 +727,7 @@ function crypto_sign_hash(sm, keypair, data, hasher) {
   reduce(seededHash);
   scalarbase(p, seededHash);
   pack(sm, p);
-  
+
   hasher.reset();
   hasher.update(sm.slice(0, 32));
   hasher.update(keypair.publicKey)
@@ -735,7 +735,7 @@ function crypto_sign_hash(sm, keypair, data, hasher) {
   hasher.finalize(result);
 
   reduce(result);
-  
+
 
   // muladd - this is from original tweetnacl-js
   var x = new Float64Array(64);
@@ -900,7 +900,7 @@ function unpack(r, p) {
   M(r[3], r[0], r[1]);
   return 0;
 }
-var 
+var
     crypto_scalarmult_BYTES = 32,
     crypto_scalarmult_SCALARBYTES = 32,
     crypto_sign_BYTES = 64,
@@ -1039,8 +1039,13 @@ nacl.pack = pack;
       });
     }
   } else if (typeof require !== 'undefined') {
-    // Node.js.
-    crypto = require('crypto');
+    // Node.js or ReactNative
+    try {
+      var rn = require.resolve('react-native');
+    } catch (e) {
+      var rn = undefined;
+    }
+    crypto = rn ? require('react-native-crypto') : require('crypto');
     if (crypto) {
       nacl.setPRNG(function(x, n) {
         var i, v = crypto.randomBytes(n);
